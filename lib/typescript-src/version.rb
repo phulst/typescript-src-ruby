17f4cd6cd5edf4eb627837bbf75889af6109303d
MATSUMOTO, Katsuyoshi <github@katsyoshi.org>
2015-01-26 00:58:51 +0900

    import v1.4

--- lib/typescript-src/version.rb
+++ lib/typescript-src/version.rb
@@ -1,5 +1,5 @@
 module TypeScript
   module Src
-    VERSION = '1.0.1.2' # TypeScript compiler version + gem's revision
+    VERSION = '1.4.0.custom' # TypeScript compiler version + gem's revision
   end
 end
