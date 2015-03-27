942a1dc72541254787b888eb0d0314678a8a2d3d
Peter Hulst <phulst@sbcglobal.net>
2015-03-27 00:48:13 -0700

    changed to 1.5.0

--- lib/typescript-src/version.rb
+++ lib/typescript-src/version.rb
@@ -1,5 +1,5 @@
 module TypeScript
   module Src
-    VERSION = '1.5.0.ph' # TypeScript compiler version + gem's revision
+    VERSION = '1.5.0' # TypeScript compiler version + gem's revision
   end
 end
